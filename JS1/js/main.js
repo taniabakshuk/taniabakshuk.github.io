0e4b94fbcd1b03be1edc88096df6b9fcb8aa70f0
tania.bakshuk <tania.bakshuk@gmail.com>
2020-07-06 10:27:57 +0300

    3

--- JS1/js/main.js
+++ JS1/js/main.js
@@ -1,26 +1,24 @@
 
+const $body = document.querySelector('#body');
 const $login = document.querySelector("#login");
 const $todo = document.querySelector("#todo");
 const $modalDelete = document.querySelector("#modal-delete");
 const $modalEdit = document.querySelector("#modal-edit");
-const $btnLogin = document.querySelector('.login__submit');
-const btnAddTask = document.querySelector('#btn-add-task');
-const $body = document.querySelector('#body');
-const btnDeleteNo = document.querySelector('#btn-delete-no');
+let $inputTask = document.querySelector('.todo__input-task');
 
-let year = new  Date().getFullYear();
-let date = new Date().getDate();
-if (date < 10) date = '0' + date;
-let month = new Date().getMonth() + 1;
-if (month < 10) month = '0' + month;
-let hour = new Date().getHours();
-let minute = new Date().getMinutes();
+const $btnLogin = document.querySelector('.login__submit');
+const $btnAddTask = document.querySelector('#btn-add-task');
+const $btnEditCancel = document.querySelector('#btn-edit-cancel');
+const $btnDeleteNo = document.querySelector('#btn-delete-no');
+
+let taskList = [];
 
 
 $btnLogin.addEventListener('click', getLoginValue);
 $btnLogin.addEventListener('click', validation);
-btnAddTask.addEventListener('click', addTask);
-btnDeleteNo.addEventListener('click', hideModalDelete);
+$btnAddTask.addEventListener('click', addTask);
+$btnDeleteNo.addEventListener('click', hideModalDelete);
+$btnEditCancel.addEventListener('click', hideModalEdit);
 
 
 function getLoginValue(event) {
@@ -50,22 +48,34 @@ function validation() {
 }
 
 function addTask() {
+    const newTask = {
+        year: new Date().getFullYear(),
+        date: new Date().getDate(),
+        month: new Date().getMonth() + 1,
+        hour: new Date().getHours(),
+        minute: new Date().getMinutes(),
+        todo: $inputTask.value,
+        priority: 1,
+    };
+    taskList.push(newTask);
+    $inputTask.value = '';
     let $task = document.createElement('div');
+    if (newTask.todo.length === 0) $task = '';
     $task.className = 'task';
     $task.innerHTML = `            <div class="task__box-left">
             <div class="task__wrap">
-                <time class="task__time">${date}.${month}.${year}</time>
-                <time class="task__time">${hour}:${minute}</time>
+                <time class="task__time">${newTask.date = (newTask.date < 10) ? '0' + newTask.date : false}.${newTask.month = (newTask.month < 10) ? '0' + newTask.month : false}.${newTask.year}</time>
+                <time class="task__time">${newTask.hour}:${newTask.minute}</time>
                 </div>
-                <div class="task__priority">1</div>
+                <div class="task__priority">${newTask.priority}</div>
                 <div class="task__wrap">
                     <button class="task__priority-button"><i class="fas fa-chevron-up"></i></button>
                     <button class="task__priority-button"><i class="fas fa-chevron-down"></i></button>
                 </div>
             </div>
-            <p class="task__text"></p>
+            <p class="task__text">${newTask.todo}</p>
             <div class="task__box-right">
-                <button class="task__button task__button_green"><i class="fas fa-pencil-alt"></i></button>
+                <button class="task__button task__button_green" onClick="showModalEdit();"><i class="fas fa-pencil-alt"></i></button>
                 <button class="task__button"><i class="fas fa-check"></i></button>
                 <button class="task__button" onClick="showModalDelete();"><i class="fas fa-trash-alt"></i></button>
             </div>`;
@@ -79,6 +89,16 @@ function showModalDelete() {
 
 function hideModalDelete() {
     $modalDelete.classList.remove('modal-delete_show');
+    $body.classList.remove('body_hidden');
+}
+
+function showModalEdit() {
+    $modalEdit.classList.add('modal-edit_show');
+    $body.classList.add('body_hidden');
+}
+
+function hideModalEdit() {
+    $modalEdit.classList.remove('modal-edit_show');
     $body.classList.remove('body_hidden');
 }
 
